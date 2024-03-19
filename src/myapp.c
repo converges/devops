847191c342365d3603e2d9c0f300febe61ec5d60
Anu <mindtheorigin@gmail.com>
2024-03-19 16:22:03 +0900

    fix testing main dupl prob

--- src/myapp.c
+++ src/myapp.c
@@ -1,22 +1,16 @@
+#include "myapp.h"
 #include <stdio.h>
-
-int add(int a, int b);
-int mul(int a, int b);
-
-
-int main() {
-    printf("Hello, World!\n");
-
-    int sum = add(3, 4);
-
-    
-    return 0;
-}
 
 int add(int a, int b) {
     return a + b;
 }
 
-int sub(int a, int b) { // depends on the add function
+int sub(int a, int b) {
     return add(a, -b);
+}
+
+int main() {
+    printf("Hello, World!\n");
+    int sum = add(3, 4);
+    return 0;
 }
---
+++ src/myapp.h
@@ -0,0 +1,7 @@
+#ifndef MYAPP_H
+#define MYAPP_H
+
+int add(int a, int b);
+int sub(int a, int b);
+
+#endif
