0218f1fd9dcca9545064790f8d46bab12788efca
Anu <mindtheorigin@gmail.com>
2024-03-19 16:27:06 +0900

    Update unit test

--- tests/unit_test.c
+++ tests/unit_test.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
 
-// 파일 경로
 #define REPORT_FILE_PATH "reports/myapp_testing.txt"
-
-#include "../src/myapp.c"
+#include "../src/myapp.h"
 
 void test_add();
 void test_sub();
 void initialize_testing_file();
+void log_test_result(FILE *fp, int condition, char *testName);
 
 int main() {
     initialize_testing_file(); // report 파일 초기화
@@ -19,41 +18,49 @@ int main() {
     return 0;
 }
 
-// report 파일 초기화 함수
 void initialize_testing_file() {
-    FILE *fp;
-    fp = fopen(REPORT_FILE_PATH, "w");
-    assert(fp != NULL);
+    FILE *fp = fopen(REPORT_FILE_PATH, "w");
+    if (fp == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
     fprintf(fp, "------------------------------------\n");
     fprintf(fp, "Running Unit Tests...\n");
     fclose(fp);
 }
 
-// add 함수 테스트 함수
 void test_add() {
-    FILE *fp;
-    fp = fopen(REPORT_FILE_PATH, "a");
-    assert(fp != NULL);
+    FILE *fp = fopen(REPORT_FILE_PATH, "a");
+    if (fp == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
 
-
-    assert(add(2, 3) == 5);
-    assert(add(19, -1) == 18);
-    assert(add(-3, -2) == -5);
-    fprintf(fp, "test: add passed\n");
+    log_test_result(fp, add(2, 3) == 5, "add(2, 3) == 5");
+    log_test_result(fp, add(19, -1) == 18, "add(19, -1) == 18");
+    log_test_result(fp, add(-3, -2) == -5, "add(-3, -2) == -5");
 
     fclose(fp);
 }
 
-// sub 함수 테스트 함수
 void test_sub() {
-    FILE *fp;
-    fp = fopen(REPORT_FILE_PATH, "a");
-    assert(fp != NULL);
+    FILE *fp = fopen(REPORT_FILE_PATH, "a");
+    if (fp == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
 
-    assert(sub(2, 3) == -1);
-    assert(sub(19, -1) == 20);
-    assert(sub(-3, -2) == -1);
-    fprintf(fp, "test: sub passed\n");
+    log_test_result(fp, sub(2, 3) == -1, "sub(2, 3) == -1");
+    log_test_result(fp, sub(19, -1) == 20, "sub(19, -1) == 20");
+    log_test_result(fp, sub(-3, -2) == -1, "sub(-3, -2) == -1");
 
     fclose(fp);
+}
+
+void log_test_result(FILE *fp, int condition, char *testName) {
+    if (condition) {
+        fprintf(fp, "Test Passed: %s\n", testName);
+    } else {
+        fprintf(fp, "Test FAILED: %s\n", testName);
+    }
 }
